5a48f5c6ea3691edb6472d4ea460f794ce7d8c0d
i-lab user <i-labuser@SDPL88537-iMacProLO.local>
2019-10-29 17:55:12 -0700

    nice

---
+++ src/_02_unbirthday/unhappybirthday.java
@@ -0,0 +1,17 @@
+package _02_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class unhappybirthday  {
+	
+	public static void main(String[] args) {
+	String bday=JOptionPane.showInputDialog("when is your bday");
+	if(bday.equals("10/29")){
+	  JOptionPane.showMessageDialog(null,"happybday"); 
+	}else "un happy bday"{
+	JOptionPane.showMessageDialog(null,"un happy bday");
+	}
+	
+	
+
+}
